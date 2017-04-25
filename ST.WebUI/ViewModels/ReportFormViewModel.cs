0c72ccaaa688430c2095e1a61ec359cc5cfa4001
aperepeliak <aperepelyak@gmail.com>
2017-04-25 08:44:07 +0300

    working on report form

---
+++ ST.WebUI/ViewModels/ReportFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ST.BLL.DTOs;
+using System.Collections.Generic;
+
+namespace ST.WebUI.ViewModels
+{
+    public class ReportFormViewModel
+    {
+
+        public IEnumerable<DeveloperDto> SelectedDevelopers { get; set; }
+    }
+}
--- ST.WebUI/ViewModels/SkillRatingFormViewModel.cs
+++ ST.WebUI/ViewModels/SkillRatingFormViewModel.cs
@@ -6,15 +6,13 @@ namespace ST.WebUI.ViewModels
 {
     public class SkillRatingFormViewModel
     {
-        public IEnumerable<CategoryDto> Categories { get; set; }
-
         [Display(Name = "Select Category")]
         public int CategoryId { get; set; }
-
-        public IEnumerable<SkillDto> Skills { get; set; }
+        public IEnumerable<CategoryDto> Categories { get; set; }
 
         [Display(Name = "Select Skill")]
         public int SkillId { get; set; }
+        public IEnumerable<SkillDto> Skills { get; set; }
 
         [Range(1, 5)]
         public int Rating { get; set; }
