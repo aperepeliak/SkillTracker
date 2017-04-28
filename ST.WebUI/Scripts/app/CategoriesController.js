a38e44f00dd973048bb40d272463e7ccb33be0a4
aperepeliak <aperepelyak@gmail.com>
2017-04-28 19:43:29 +0300

    modularize skills and categories views

---
+++ ST.WebUI/Scripts/app/CategoriesController.js
@@ -0,0 +1,75 @@
+﻿var CategoriesController = function () {
+    var element;
+
+    var init = function () {
+        $("span.delete").click(onDelete);
+        $('#category-form').submit(onSubmit);
+    };
+
+    var onSubmit = function (e) {
+        e.preventDefault();
+        var categoryName = $('.category-name').val();
+        validateAndSubmitCategory(categoryName);    
+    };
+    var onDelete = function (e) {
+        e.preventDefault();
+        element = $(e.target);
+        var categoryId = element.attr("data-category-id");
+        var categoryName = element.closest('td').prev().text().trim();
+
+        bootbox.confirm({
+            message: "Are you sure you want to delete <strong>" + categoryName + "</strong> category?",
+            buttons: {
+                cancel: {
+                    label: 'No',
+                    className: 'btn-default'
+                },
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                if (!result) return;
+                deleteCategory(categoryId);
+            }
+        });
+    }
+
+    var validateAndSubmitCategory = function (categoryName) {
+        $.ajax({
+            url: `/api/categories/validateUnique?name=${categoryName}`,
+            method: 'GET'
+        })
+            .done(function (response) {
+                if (!response) {
+                    var message = $('<div class="alert alert-danger" role="alert">There is already a category with such name.</div>')
+                    $('.alert-info').after(message);
+                    message.fadeOut(4000);
+                } else {
+                    $('#category-form').off('submit');
+                    $('#category-form').submit();
+                }
+            })
+            .fail(fail);
+    }
+    var deleteCategory = function (categoryId) {
+        $.ajax({
+            url: "/categories/delete/" + categoryId,
+            method: "POST"
+        })
+            .done(function () {
+                element.closest('tr')
+                    .fadeOut(function () {
+                        $(this).remove();
+                    })
+            })
+            .fail(fail);
+    }
+
+    var fail = function () { alert("Error!"); };
+
+    return {
+        init
+    };
+}();
---
+++ ST.WebUI/Scripts/app/SkillsController.js
@@ -0,0 +1,75 @@
+﻿var SkillsController = function () {
+    var element;
+
+    var init = function () {
+        $("span.delete").click(onDeleteSkill);
+        $('#skill-form').submit(onSubmit);
+    };
+
+    var onSubmit = function (e) {
+        e.preventDefault();
+        var skillName = $('#skill-name').val();
+        var categoryId = $('.category').val();
+
+        validateAndSubmitSkill(skillName, categoryId);
+    };
+    var onDeleteSkill = function (e) {
+        e.preventDefault();
+        element = $(e.target);
+        var skillId = element.attr("data-skill-id");
+        var skillName = element.closest('td').prev().prev().text().trim();
+
+        bootbox.confirm({
+            message: "Are you sure you want to delete <strong>" + skillName + "</strong> skill?",
+            buttons: {
+                cancel: {
+                    label: 'No',
+                    className: 'btn-default'
+                },
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                if (!result) return;
+                deleteSkill(skillId);
+            }
+        });
+    };
+
+    var validateAndSubmitSkill = function (skillName, categoryId) {
+        $.ajax({
+            url: `/api/skills/validateUnique?name=${skillName}&categoryId=${categoryId}`,
+            method: 'GET'
+        })
+            .done(function (response) {
+                if (!response) {
+                    var message = $('<div class="alert alert-danger" role="alert">There is already a skill with such name.</div>')
+                    $('.alert-info').after(message);
+                    message.fadeOut(4000);
+                } else {
+                    $('#skill-form').off('submit');
+                    $('#skill-form').submit();
+                }
+            })
+            .fail(fail);
+    }
+    var deleteSkill = function (skillId) {
+        $.ajax({
+            url: "/skills/delete/" + skillId,
+            method: "POST"
+        })
+            .done(function () {
+                element.closest('tr')
+                    .fadeOut(function () { $(this).remove(); })
+            })
+            .fail(fail);
+    }
+
+    var fail = function () { alert("Error!"); };
+
+    return {
+        init
+    };
+}();
